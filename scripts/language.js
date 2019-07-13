09b8f2817cd455586a77882651d41b28ac83b384
Chris Milson <chris.w.milson@gmail.com>
2019-07-13 14:23:41 +0900

    Change language handling

--- scripts/language.js
+++ scripts/language.js
@@ -3,6 +3,7 @@
   Default to english if no cookie exists.
 */
 
+const languages = ['en', 'ja'];
 
 var getLanguage = function() {
   var clang = document.cookie.split(';').find(c => {
@@ -22,70 +23,18 @@ It also controls the language-menu elements on the page.
 */
 
 var setLanguage = (function () {
-  var availableLanguages = new Map();
-
-  // Render the language menu
-  var resetLangMenu = function() {
-    var renderMenu = function(lang) {
-      return `
-      <li onclick="setLanguage('${ lang.language }')">
-        ${ lang.serviceName }
-      </li>
-      `;
-    };
-    var list = "";
-    availableLanguages.forEach(lang => {
-      list += renderMenu(lang);
-    });
-    var menu = document.getElementsByClassName('language-menu');
-    for (var i = 0; i < menu.length; i++) {
-      menu[i].innerHTML = '<ul>' + list + '</ul>';
-      menu[i].style = "display: initial"
+  return function(langCode) {
+    var hideAll = document.querySelectorAll('[lang]');
+    for (var i = 0; i < hideAll.length; i++) {
+      hideAll[i].style.display = 'none';
     }
-  };
 
-  if (window.XMLHttpRequest) var request = new XMLHttpRequest();
-  else var request = new ActiveXObject('Microsoft.XMLHTTP');
-
-  request.open('GET', 'languages/');
-  request.send();
-
-  request.onreadystatechange = function() {
-    if (request.readyState === 4) {
-      // request.responseText is a folder listing
-      // we will find the lisings with regex and do a request for them.
-      var regex = /href="(.*?)"/g;
-
-      var langFiles = request.responseText.match(/href=".*?"/g).map(e => {
-        // Cut off full matched string.
-        // Looks like ['en.json', 'en']
-        return /"((.*?).json)"/.exec(e).slice(1);
-      });
-
-      langFiles.forEach(fileName => {
-        if (window.XMLHttpRequest) var langRequest = new XMLHttpRequest();
-        else var langRequest = new ActiveXObject('Microsoft.XMLHTTP');
-
-        langRequest.open('GET', 'languages/' + fileName[0]);
-        langRequest.send();
-
-        langRequest.onreadystatechange = function() {
-          if (langRequest.readyState === 4) {
-            var langData = JSON.parse(langRequest.responseText);
-            availableLanguages.set(langData.language, langData);
-            resetLangMenu();
-          }
-        };
-      });
+    var show = document.querySelectorAll('[lang=' + langCode + ']');
+    for (var i = 0; i < show.length; i++) {
+      show[i].style.display = 'inherit';
     }
-  };
 
-  return function(langCode = 'en') {
     document.cookie = 'lang=' + langCode + ';max-age=' + 60 * 60 * 24 * 365;
-
-    populateContent(availableLanguages.get(langCode));
-    renderNav(availableLanguages.get(langCode));
-    renderHeader(availableLanguages.get(langCode));
   }
 })();
 
--- scripts/populate-content.js
+++ scripts/populate-content.js
@@ -6,20 +6,24 @@
 var populateContent = (function() {
   var posts = [];
 
-  var renderPost = function(post, lang) {
-    if (!lang) {
-      return;
-    }
-
+  var renderPost = function(post) {
     return `
       <div class="container post">
         <div class="title">
-          <h2>${ post[lang.language].title }</h2>
+          <h2>
+            ${
+              languages.map(lang => {
+                return '<span lang="'+lang+'">'+post[lang].title+'</span>';
+              }).join('\n')
+            }
+          </h2>
         </div>
         <div class="content">
           ${
-            post[lang.language].body.map((p) => {
-              return '<p>\n' + p + '</p>'
+            languages.map(lang => {
+              return post[lang].body.map(p => {
+                return '<p lang="' + lang + '">' + p + '</p>';
+              }).join('\n');
             }).join('\n')
           }
         </div>
@@ -30,38 +34,18 @@ var populateContent = (function() {
   if (window.XMLHttpRequest) var request = new XMLHttpRequest();
   else var request = new ActiveXObject('Microsoft.XMLHTTP');
 
-  request.open('GET', 'posts/');
+  request.open('GET', 'posts/2019-7-7.json');
   request.send();
 
   request.onreadystatechange = function() {
-    if (request.readyState === 4) {
-      // request.responseText is a folder listing
-      // we will find the lisings with regex and do a request for them.
-      var regex = /href="(.*?)"/g;
-
-      var postFiles = request.responseText.match(/href=".*?"/g).map(e => {
-        return /"((.*?).json)"/.exec(e).slice(1); // cut off full matched string
-      });
-
-      postFiles.forEach(fileName => {
-        if (window.XMLHttpRequest) var postRequest = new XMLHttpRequest();
-        else var postRequest = new ActiveXObject('Microsoft.XMLHTTP');
-
-        postRequest.open("GET", 'posts/' + fileName[0]);
-        postRequest.send();
-
-        postRequest.onreadystatechange = function() {
-          if (postRequest.readyState === 4) {
-            posts.push(JSON.parse(postRequest.responseText));
-          }
-        }
-      });
+    if (request.readyState == 4) {
+      var post = JSON.parse(request.responseText);
+      posts.push(renderPost(post))
+      populateContent();
     }
-  }
+  };
 
-  return function(lang) {
-    document.getElementById("content-main").innerHTML = posts.map(p => {
-      return renderPost(p, lang);
-    }).join('');
+  return function() {
+    document.getElementById("content-main").innerHTML = posts.join('');
   };
 })();
