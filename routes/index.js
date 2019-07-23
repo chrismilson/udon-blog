9dcc8ee65968ae43c4a73b34c56b54e74187ba85
Chris Milson <chris.w.milson@gmail.com>
2019-07-23 10:09:38 +0900

    Build old website skeleton

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Udon Blog',
+    nav: [
+      {
+        name: 'Home',
+        href: '/'
+      },
+      {
+        name: 'About',
+        href: '/about'
+      }
+    ],
+    langs: [
+      {
+        serviceName: 'English',
+        abbr: 'en'
+      },
+      {
+        serviceName: '日本語',
+        abbr: 'ja'
+      }
+    ]
+  });
 });
 
 module.exports = router;
